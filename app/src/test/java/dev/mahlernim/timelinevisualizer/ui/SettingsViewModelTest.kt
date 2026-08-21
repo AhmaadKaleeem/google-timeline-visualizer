0946e54bcb8a5c911f11b8b23b504bb149cc787a
mahler83 <33252024+SangzinAhn@users.noreply.github.com>
2026-08-21 21:30:46 +0900

    Pin ViewModel preference test to API 35

--- app/src/test/java/dev/mahlernim/timelinevisualizer/ui/SettingsViewModelTest.kt
+++ app/src/test/java/dev/mahlernim/timelinevisualizer/ui/SettingsViewModelTest.kt
@@ -14,8 +14,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class SettingsViewModelTest {
     private lateinit var cameraPreferences: CameraSettingsPreferences
     private lateinit var distancePreferences: DistanceUnitPreferences
