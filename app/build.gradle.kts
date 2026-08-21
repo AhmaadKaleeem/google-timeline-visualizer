eb702583d68f2184166830081fe66eb0fc67c2c2
mahler83 <33252024+SangzinAhn@users.noreply.github.com>
2026-08-21 20:12:59 +0900

    Prepare Timeline Visualizer 2.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.mahlernim.timelinevisualizer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "2.2.6"
+        versionCode = 26
+        versionName = "2.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
